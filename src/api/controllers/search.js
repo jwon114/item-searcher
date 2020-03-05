417903f657454a057746cd20cc1bf313fcd2426f
jwon114 <jthgwong@gmail.com>
2020-03-05 23:03:05 +0000

    searchItem function, sql fetch data

--- src/api/controllers/search.js
+++ src/api/controllers/search.js
@@ -2,17 +2,23 @@ const searchItems = require('../../../db/searchItems')
 
 const search = (req, res) => {
   let status, response = {}
-  if (req.query.searchTerm) {
-    status = 200
-    response["result"] = searchItems(req.query.searchTerm)
-  } else {
+  if (!req.query.searchTerm) {
     status = 400
+    response["status"] = status
     response["error"] = 'Required query param missing'
+    res.status(status)
+    res.send(response)
   }
 
-  response["status"] = status
-  res.status(status)
-  res.send(response)
+  searchItems(req.query.searchTerm)
+    .then(result => {
+      status = 200
+      response["status"] = status
+      response["result"] = result.rows
+      res.status(status)
+      res.send(response)
+    })
+    .catch(e => console.error(e))
 }
 
 module.exports = { search }
